63eab54ce8d0b1673d79a53a4e8606c0228b29d8
Majid Moghadam <majid.moghadam114@yahoo.com>
2021-09-18 18:18:42 -0400

    Chapter 4 - SpaceMarine turns towards around towards mouse pointer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     private CharacterController characterController;
     public Rigidbody head;
 
+    public LayerMask layerMask;
+    private Vector3 currentLookTarget = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +33,29 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
+
+        if (Physics.Raycast(ray, out hit, 1000, layerMask,QueryTriggerInteraction.Ignore))
+        {
+            if (hit.point != currentLookTarget)
+            {
+                currentLookTarget = hit.point;
+            }
+
+            // 1
+            Vector3 targetPosition = new Vector3(hit.point.x,
+            transform.position.y, hit.point.z);
+            // 2
+            Quaternion rotation = Quaternion.LookRotation(targetPosition -
+            transform.position);
+            // 3
+            transform.rotation = Quaternion.Lerp(transform.rotation,
+            rotation, Time.deltaTime * 10.0f);
+        }
+
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
         if (moveDirection == Vector3.zero)
         {
