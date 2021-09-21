709c30b7a144357c2f87eb8106dea37438a6398c
Majid Moghadam <majid.moghadam114@yahoo.com>
2021-09-21 16:53:52 -0400

    Chapter 4 - got the Space Marine's head bobbling

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,17 +36,31 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;
     private CharacterController characterController;
+    public Rigidbody head;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         characterController = GetComponent<CharacterController>();
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
-        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (moveDirection == Vector3.zero)
+        {
+            // TODO
+        }
+        else;
+        {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
     }
 }
