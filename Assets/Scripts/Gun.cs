624430fc6262801058eb93a1fea2119dd0e50254
Majid Moghadam <majid.moghadam114@yahoo.com>
2021-10-03 16:49:38 -0400

    chapter 7 - added PowerUps with sound

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -38,6 +38,10 @@ public class Gun : MonoBehaviour
     public Transform launchPosition;
     private AudioSource audioSource;
 
+    public bool isUpgraded;
+    public float upgradeTime = 10.0f;
+    private float currentTime;
+
     // Use this for initialization
     void Start ()
     {
@@ -58,14 +62,49 @@ public class Gun : MonoBehaviour
         {
             CancelInvoke("fireBullet");
         }
-	}
+
+        currentTime += Time.deltaTime;
+        if (currentTime > upgradeTime && isUpgraded == true)
+        {
+            isUpgraded = false;
+        }
+    }
 
     void fireBullet()
     {
+        Rigidbody bullet = createBullet();
+        bullet.velocity = transform.parent.forward * 100;
+
+        if (isUpgraded)
+        {
+            Rigidbody bullet2 = createBullet();
+            bullet2.velocity =
+            (transform.right + transform.forward / 0.5f) * 100;
+            Rigidbody bullet3 = createBullet();
+            bullet3.velocity =
+            ((transform.right * -1) + transform.forward / 0.5f) * 100;
+        }
+
+        if (isUpgraded)
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.upgradedGunFire);
+        }
+        else
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+        }
+    }
+
+    private Rigidbody createBullet()
+    {
         GameObject bullet = Instantiate(bulletPrefab) as GameObject;
         bullet.transform.position = launchPosition.position;
-        bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
+        return bullet.GetComponent<Rigidbody>();
+    }
 
-        audioSource.PlayOneShot(SoundManager.Instance.gunFire);
+    public void UpgradeGun()
+    {
+        isUpgraded = true;
+        currentTime = 0;
     }
 }
