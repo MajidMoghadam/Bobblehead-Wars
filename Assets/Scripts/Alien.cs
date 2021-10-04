269222fee1aca6a5e5509033b3b2f9601b9872c3
Majid Moghadam <majid.moghadam114@yahoo.com>
2021-10-03 23:16:53 -0400

    chapter 8 - Destroyed Alien gameobjects using the Event System

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Events;
 
 public class Alien : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class Alien : MonoBehaviour
     private NavMeshAgent agent;
     public float navigationUpdate;
     private float navigationTime = 0;
+    public UnityEvent OnDestroy;
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +34,16 @@ public class Alien : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
+        Die();
         SoundManager.Instance.PlayOneShot(SoundManager.Instance.alienDeath);
+    }
+
+   
+
+    public void Die()
+    {
+        OnDestroy.Invoke();
+        OnDestroy.RemoveAllListeners();
+        Destroy(gameObject);
     }
 }
